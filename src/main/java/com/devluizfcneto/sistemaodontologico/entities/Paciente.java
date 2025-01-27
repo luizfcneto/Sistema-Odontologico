d1bcb150dd143650f45257eadce2948be0337a56
luizfcneto <luizfcneto123@gmail.com>
2025-01-27 03:55:17 -0300

    +Adicioando entidades simples do projeto

---
+++ src/main/java/com/devluizfcneto/sistemaodontologico/entities/Paciente.java
@@ -0,0 +1,94 @@
+package com.devluizfcneto.sistemaodontologico.entities;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "paciente")
+public class Paciente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	private String cpf;
+	private String nome;
+	
+	@Column(name = "data_nascimento")
+	private LocalDate dataNascimento;
+	
+	public Paciente(String cpf, String nome, LocalDate dataNascimento) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+	public Paciente(Long id, String cpf, String nome, LocalDate dataNascimento) {
+		this.id = id;
+		this.cpf = cpf;
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Paciente other = (Paciente) obj;
+		return Objects.equals(cpf, other.cpf);
+	} 
+		
+}
