a99e85c70af9a2a733f0426b8b73bd7d8450152a
luizfcneto <luizfcneto123@gmail.com>
2025-02-11 03:51:26 -0300

    +Criando camada de testes unitarios com JUnit e Mockito

---
+++ src/test/java/com/devluizfcneto/sistemaodontologico/AllTests.java
@@ -0,0 +1,14 @@
+package com.devluizfcneto.sistemaodontologico;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.devluizfcneto")
+@SelectClasses({	
+
+})
+public class AllTests {
+
+}
