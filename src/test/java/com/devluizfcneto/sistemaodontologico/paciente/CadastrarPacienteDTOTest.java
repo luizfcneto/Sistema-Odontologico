43faa90557c42361a1804477c7671625bfb20a9c
luizfcneto <luizfcneto123@gmail.com>
2025-02-17 19:47:13 -0300

    *Refatorando testes de Paciente

--- src/test/java/com/devluizfcneto/sistemaodontologico/paciente/CadastrarPacienteDTOTest.java
+++ src/test/java/com/devluizfcneto/sistemaodontologico/paciente/CadastrarPacienteDTOTest.java
@@ -1,131 +1,50 @@
 package com.devluizfcneto.sistemaodontologico.paciente;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.devluizfcneto.sistemaodontologico.dtos.CadastrarPacienteDTO;
-import com.devluizfcneto.sistemaodontologico.entities.Paciente;
-import com.devluizfcneto.sistemaodontologico.errors.BadRequestException;
 
 public class CadastrarPacienteDTOTest {
 
-	private CadastrarPacienteDTO pacienteDTO;
-	
-	@BeforeEach
-	void setUp() {
-		pacienteDTO = new CadastrarPacienteDTO();
-	}
-	
-	@Test
-    public void testCalculaIdadeComDataNascimentoValida() {
-        String dataNascimento = "10/05/1990";
-        this.pacienteDTO = new CadastrarPacienteDTO();
-        this.pacienteDTO.setDataNascimento(dataNascimento);
-        this.pacienteDTO.calculaIdade();
-
-        int idadeEsperada = LocalDate.now().getYear() - 1990;
-        assertEquals(idadeEsperada, this.pacienteDTO.getIdade());
+private CadastrarPacienteDTO pacienteDTO;
+    
+    @BeforeEach
+    void setUp() {
+        pacienteDTO = new CadastrarPacienteDTO();
     }
 
     @Test
-    @DisplayName("Testando calcular idade do paciente com data de nascimento nulo")
-    public void testCalculaIdadeComDataNascimentoNula() {
-        this.pacienteDTO = new CadastrarPacienteDTO();
-//        assertThrows(BadRequestException.class, this.pacienteDTO::calculaIdade);
-        assertThrows(BadRequestException.class, () -> this.pacienteDTO.calculaIdade());
-    }
-
-    @Test
-    @DisplayName("Testando construcao do paciente com construtor da Entidade Paciente")
-    public void testConstrutorComPaciente() {
-        LocalDate dataNascimento = LocalDate.of(1990, 5, 10);
-        Paciente paciente = new Paciente("12345678900", "Nome do Paciente", dataNascimento);
-        this.pacienteDTO = new CadastrarPacienteDTO(paciente);
-
-        assertEquals("12345678900", this.pacienteDTO.getCpf());
-        assertEquals("Nome do Paciente", this.pacienteDTO.getNome());
-        assertEquals("10/5/1990", this.pacienteDTO.getDataNascimento());
-
-        int idadeEsperada = LocalDate.now().getYear() - 1990;
-        assertEquals(idadeEsperada, this.pacienteDTO.getIdade());
-    }
-
-    @Test
-    @DisplayName("Testando construcao do paciente com construtor passando variaveis")
-    public void testConstrutorComCpfNomeDataNascimento() {
+    @DisplayName("Testar construção com parâmetros diretos")
+    public void testConstrutorComParametros() {
         String cpf = "98765432100";
         String nome = "Outro Nome";
         String dataNascimento = "20/02/2000";
-
-        this.pacienteDTO = new CadastrarPacienteDTO(cpf, nome, dataNascimento);
-
-        assertEquals(cpf, this.pacienteDTO.getCpf());
-        assertEquals(nome, this.pacienteDTO.getNome());
-        assertEquals(dataNascimento, this.pacienteDTO.getDataNascimento());
-
-        int idadeEsperada = LocalDate.now().getYear() - 2000;
-        assertEquals(idadeEsperada, this.pacienteDTO.getIdade());
+        
+        CadastrarPacienteDTO dto = new CadastrarPacienteDTO(cpf, nome, dataNascimento);
+        
+        assertEquals(cpf, dto.getCpf());
+        assertEquals(nome, dto.getNome());
+        assertEquals(dataNascimento, dto.getDataNascimento());
     }
-    
+
     @Test
-    @DisplayName("Testando construcao do paciente setando variaveis")
+    @DisplayName("Testar setters")
     public void testSetPacienteComSucesso() {
         String cpf = "98765432100";
         String nome = "Outro Nome";
         String dataNascimento = "20/02/2000";
-
-        this.pacienteDTO = new CadastrarPacienteDTO();
-        this.pacienteDTO.setCpf(cpf);
-        this.pacienteDTO.setNome(nome);
-        this.pacienteDTO.setDataNascimento(dataNascimento);
-        this.pacienteDTO.calculaIdade();
         
-        assertEquals(cpf, this.pacienteDTO.getCpf());
-        assertEquals(nome, this.pacienteDTO.getNome());
-        assertEquals(dataNascimento, this.pacienteDTO.getDataNascimento());
+        pacienteDTO.setCpf(cpf);
+        pacienteDTO.setNome(nome);
+        pacienteDTO.setDataNascimento(dataNascimento);
         
-        int idadeEsperada = LocalDate.now().getYear() - 2000;
-        assertEquals(idadeEsperada, this.pacienteDTO.getIdade());
-    }
-
-    @Test
-    @DisplayName("Testando calcular idade com data de nascimento com formato invalido")
-    public void testCalculaIdadeComDataNascimentoFormatoInvalido() {
-        String dataNascimento = "10-05-1990";
-        this.pacienteDTO = new CadastrarPacienteDTO();
-        this.pacienteDTO.setDataNascimento(dataNascimento);
-
-//        assertThrows(BadRequestException.class, this.pacienteDTO::calculaIdade);
-        assertThrows(BadRequestException.class, () -> this.pacienteDTO.calculaIdade() );
-    }
-
-    @Test
-    @DisplayName("Testando calcular idade com data de nascimento com dia invalido")
-    public void testCalculaIdadeComDataNascimentoDiaInvalido() {
-        String dataNascimento = "32/05/1990"; 
-        this.pacienteDTO = new CadastrarPacienteDTO();
-        this.pacienteDTO.setDataNascimento(dataNascimento);
-
-//        assertThrows(BadRequestException.class, this.pacienteDTO::calculaIdade);
-        assertThrows(BadRequestException.class, () -> this.pacienteDTO.calculaIdade() );
-    }
-
-    @Test
-    @DisplayName("Testando calcular idade com data de nascimento com mês invalido")
-    public void testCalculaIdadeComDataNascimentoMesInvalido() {
-        String dataNascimento = "10/13/1990";
-        this.pacienteDTO = new CadastrarPacienteDTO();
-        this.pacienteDTO.setDataNascimento(dataNascimento);
-
-//        assertThrows(BadRequestException.class, this.pacienteDTO::calculaIdadeas);
-        assertThrows(BadRequestException.class, () -> this.pacienteDTO.calculaIdade() );
-        
+        assertEquals(cpf, pacienteDTO.getCpf());
+        assertEquals(nome, pacienteDTO.getNome());
+        assertEquals(dataNascimento, pacienteDTO.getDataNascimento());
     }
 	
 }
--- src/test/java/com/devluizfcneto/sistemaodontologico/paciente/ControllerRemoverPacienteTest.java
+++ src/test/java/com/devluizfcneto/sistemaodontologico/paciente/ControllerRemoverPacienteTest.java
@@ -1,74 +1,87 @@
 package com.devluizfcneto.sistemaodontologico.paciente;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.devluizfcneto.sistemaodontologico.controllers.PacienteController;
-import com.devluizfcneto.sistemaodontologico.dtos.ErrorDTO;
+import com.devluizfcneto.sistemaodontologico.errors.GlobalExceptionHandler;
 import com.devluizfcneto.sistemaodontologico.errors.PacienteNotFoundException;
 import com.devluizfcneto.sistemaodontologico.services.PacienteService;
 
+@ExtendWith(MockitoExtension.class)
 public class ControllerRemoverPacienteTest {
-	
-	@Mock
-	private PacienteService pacienteService;
-	
-	@InjectMocks
-	private PacienteController pacienteController;
-	
-	@BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private PacienteService pacienteService;
+
+    @InjectMocks
+    private PacienteController pacienteController;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(pacienteController)
+                .setControllerAdvice(new GlobalExceptionHandler())
+                .build();
     }
 
+    @Test
+    @DisplayName("Deve retornar 204 No Content ao remover paciente existente")
+    void removerPaciente_Sucesso() throws Exception {
+        // Arrange
+        Long id = 1L;
+        doNothing().when(pacienteService).remover(id);
 
-	@Test
-    @DisplayName("Testando remover paciente SUCESSO")
-	public void deveRemoverPacienteComSucesso() {
-	    Long id = 1L;
-	    
-	    doNothing().when(pacienteService).remover(id);
-	    ResponseEntity<?> response = pacienteController.removerPaciente(id);
-	
-	    assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
-	    verify(pacienteService, times(1)).remover(id);
-	}
-	
-	@Test
-    @DisplayName("Testando remover paciente ERRO NAO ENCONTRADO")
-	public void deveRetornarNotFoundQuandoPacienteNaoEncontrado() {
-	    Long id = 1L;
-	     
-	    doThrow(PacienteNotFoundException.class).when(pacienteService).remover(id);
-	    PacienteNotFoundException pNFE = assertThrows(PacienteNotFoundException.class, () -> pacienteController.removerPaciente(id));
-	    assertEquals(404, pNFE.getCode());
-	}
-	
-	@Test
-    @DisplayName("Testando remover paciente ERRO ERRO INESPERADO")
-	public void deveRetornarInternalServerErrorQuandoOcorrerErroInesperado() {
-	    Long id = 1L;
-	    String mensagemErro = "Erro inesperado";
-	    doThrow(new RuntimeException(mensagemErro)).when(pacienteService).remover(id);
-	   
-	    ResponseEntity<?> response = pacienteController.removerPaciente(id);
-	
-	    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-	    ErrorDTO errorDTO = (ErrorDTO) response.getBody();
-	    assert errorDTO != null;
-	    assertEquals(mensagemErro, errorDTO.getMessage());
-	}
+        // Act & Assert
+        mockMvc.perform(delete("/api/paciente/{id}", id))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        verify(pacienteService, times(1)).remover(id);
+    }
+
+    @Test
+    @DisplayName("Deve retornar 404 Not Found ao tentar remover paciente inexistente")
+    void removerPaciente_NaoEncontrado() throws Exception {
+        // Arrange
+        Long id = 999L;
+        doThrow(new PacienteNotFoundException("Paciente não encontrado"))
+            .when(pacienteService).remover(id);
+
+        // Act & Assert
+        mockMvc.perform(delete("/api/paciente/{id}", id))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Paciente não encontrado"));
+    }
+
+    @Test
+    @DisplayName("Deve retornar 500 Internal Server Error em caso de erro inesperado")
+    void removerPaciente_ErroInterno() throws Exception {
+        // Arrange
+        Long id = 1L;
+        String errorMessage = "Ocorreu um erro interno no servidor.";
+        doThrow(new RuntimeException(errorMessage))
+            .when(pacienteService).remover(id);
+
+        // Act & Assert
+        mockMvc.perform(delete("/api/paciente/{id}", id))
+                .andDo(print())
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.message").value(errorMessage));
+    }
 }
